5739f1411bd7bf47b9388eeccfb8a7da7fe59133
Chris Chiang <chris23chiang@gmail.com>
2021-05-24 20:08:13 +0800

    Test

--- line_crawler.py
+++ line_crawler.py
@@ -38,9 +38,30 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    s = '很抱歉，您說什麼?'
+
+    if '貼圖' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+        
+        line_bot_api.reply_message(event.reply_token,sticker_message)
+        return
+
+    if msg in ['hi', 'Hi']:
+        s = '嗨!'
+    elif msg == '你吃飯了嗎':
+        s = '還沒'
+    elif msg == '你是誰':
+        s = '我是機器人'
+    elif '訂位' in msg:
+        s = '你想訂位，是嗎?'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=s))
 
 
 if __name__ == "__main__":
